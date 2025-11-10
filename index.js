0cea4bc777cb6002b8c8676b650c237654ebde02
Malhar Prasad Ray <malharprasadray@gmail.com>
2025-11-10 12:16:47 +0530

    new problem statements solved

--- index.js
+++ index.js
@@ -23,3 +23,36 @@ const languages = ["JavaScript", "Python", "Java", "C++", "Ruby"];
 for (const lang of languages) {
     console.log("Language: " + lang);
 }
+
+// for in loop
+// loop through properties of an object.
+const person = { name: "John", age: 30, city: "New York" };
+for (const key in person) {
+    console.log(key + ": " + person[key]);
+}
+
+// fibonanci series
+function fibonacci(n) {
+    let a = 0, b = 1, next;
+    console.log("Fibonacci Series:");
+    for (let i = 0; i < n; i++) {
+        console.log(a);
+        next = a + b;
+        a = b;
+        b = next;
+    }
+}
+
+fibonacci(10);
+
+// for of loop
+const lang = "Javascript";
+let length = 0;
+    
+for (const i of lang) {
+    console.log(i);
+    length++;
+}
+console.log("Length of the string is: " + length);
+
+
